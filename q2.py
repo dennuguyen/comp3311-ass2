86c28b3afc6506db2c88f88250cc8eaf02ff5eff
dennuguyen <dennuguyen@gmail.com>
2023-11-11 16:21:36 +1100

    Style.

--- q2.py
+++ q2.py
@@ -5,40 +5,29 @@ import sys
 import psycopg2
 import re
 
-# define any local helper functions here
-# ...
-
-### set up some globals
-
-usage = f"Usage: {sys.argv[0]} SubjectCode"
-db = None
-
-### process command-line args
-
 argc = len(sys.argv)
+
 if argc < 2:
-  print(usage)
-  exit(1)
+    print(f"Usage: {sys.argv[0]} SubjectCode")
+    exit(1)
+
 subject = sys.argv[1]
-check = re.compile("^[A-Z]{4}[0-9]{4}$")
-if not check.match(subject):
-  print("Invalid subject code")
-  exit(1)
+if not re.compile("^[A-Z]{4}[0-9]{4}$").match(subject):
+    print("Invalid subject code")
+    exit(1)
 
-try:
-  db = psycopg2.connect("dbname=ass2")
-  subjectInfo = getSubject(db,subject)
-  if not subjectInfo:
-      print(f"Invalid subject code {code}")
-      exit(1)
-  #print(subjectInfo)  #debug
+code = None
+def row():
+  return f"{code} {satisfaction:6d} {num_responses:6d} {num_students:6d}  {convenor}"
 
-  # List satisfaction for subject over time
+with psycopg2.connect("dbname=ass2") as conn:
+    with conn.cursor() as curs:
+        subjectInfo = getSubject(conn, subject)
+        if not subjectInfo:
+            print(f"Invalid subject code {code}")
+            exit(1)
+        #print(subjectInfo)  #debug
 
-  # ... add your code here ...
+        # List satisfaction for subject over time
 
-except Exception as err:
-  print(err)
-finally:
-  if db:
-    db.close()
+        # ... add your code here ...
