3db20e1bcca8c16c1f1bf22906a90825cbacf209
dennuguyen <dennuguyen@gmail.com>
2023-11-11 16:19:45 +1100

    Style.

--- q1.py
+++ q1.py
@@ -5,56 +5,45 @@ import sys
 import psycopg2
 import re
 
-db = None
+with psycopg2.connect("dbname=ass2") as conn:
+    with conn.cursor() as curs:
+        curs.execute(
+            """
+            with student_enrolments as (
+                select distinct on (terms.code, students.id)
+                terms.code as term,
+                students.status as status
+                from terms
+                inner join program_enrolments on program_enrolments.term = terms.id
+                inner join students on students.id = program_enrolments.student
+            )
+            select * from student_enrolments order by term
+            """
+        )
 
-try:
-  db = psycopg2.connect("dbname=ass2")
-  cur = db.cursor()
+        # Print header.
+        print("Term  #Locl  #Intl Proportion")
 
-  # show term, #locals, #internationals, fraction
-  cur.execute("""
-    with student_enrolments as (
-      select distinct on (terms.code, students.id)
-        terms.code as term,
-        students.status as status
-      from terms
-      inner join program_enrolments on program_enrolments.term = terms.id
-      inner join students on students.id = program_enrolments.student
-    )
-    select * from student_enrolments order by term
-    """)
+        prev_term = "19T1"
+        num_locals = 0
+        num_intls = 0
+        
+        # Helper to print table rows.
+        def row():
+            return f"{prev_term} {num_locals:6d} {num_intls:6d} {(num_locals / num_intls if num_intls else 0):6.1f}"
 
-  # Print header.
-  print("Term  #Locl  #Intl Proportion")
+        for [term, status] in cur.fetchall():
+            if status == "INTL":
+                num_intls += 1
+            else:
+                num_locals += 1
 
-  prev_term = "19T1"
-  num_locals = 0
-  num_intls = 0
-  
-  # Helper to print table rows.
-  def row():
-    return f"{prev_term} {num_locals:6d} {num_intls:6d} {(num_locals / num_intls if num_intls else 0):6.1f}"
+            # When term changes, print the proportion so far
+            if prev_term != term:
+                print(row())
+                prev_term = term
+                num_locals = 0
+                num_intls = 0
 
-  for [term, status] in cur.fetchall():
-    if status == "INTL":
-      num_intls += 1
-    else:
-      num_locals += 1
-
-    # When term changes, print the proportion so far
-    if prev_term != term:
-      print(row())
-      prev_term = term
-      num_locals = 0
-      num_intls = 0
-
-  # Print very last row
-  print(row())
-
-  cur.close()
-
-except Exception as err:
-  print(err)
-finally:
-  if db:
-    db.close()
+        # Print very last row
+        print(row())
