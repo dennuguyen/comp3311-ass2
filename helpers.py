9517784883ea198fca6b101612cfae8349f90374
dennuguyen <dennuguyen@gmail.com>
2023-11-11 22:14:37 +1100

    Complete q3 (but please clean up and check edge cases later).

--- helpers.py
+++ helpers.py
@@ -2,39 +2,31 @@
 # add here any functions to share between Python scripts 
 # you must submit this even if you add nothing
 
-def getProgram(db,code):
-  cur = db.cursor()
-  cur.execute("select * from Programs where code = %s",[code])
-  info = cur.fetchone()
-  cur.close()
-  if not info:
-    return None
-  else:
-    return info
+def get_program_info(conn, code):
+    curs = conn.cursor()
+    curs.execute(f"select * from programs where programs.code = '{code}'")
+    info = curs.fetchone()
+    curs.close()
+    return info or None
 
-def getStream(db,code):
-  cur = db.cursor()
-  cur.execute("select * from Streams where code = %s",[code])
-  info = cur.fetchone()
-  cur.close()
-  if not info:
-    return None
-  else:
-    return info
+def get_stream_info(conn, code):
+    curs = conn.cursor()
+    curs.execute(f"select * from streams where streams.code = '{code}'")
+    info = curs.fetchone()
+    curs.close()
+    return info or None
 
-def getStudent(db,zid):
-  cur = db.cursor()
-  qry = """
-  select p.*
-  from   People p
-         join Students s on s.id = p.id
-  where  p.id = %s
-  """
-  cur.execute(qry,[zid])
-  info = cur.fetchone()
-  cur.close()
-  if not info:
-    return None
-  else:
-    return info
+def get_student_info(conn, zid):
+    curs = conn.cursor()
+    curs.execute(
+        f"""
+        select people.*
+        from people
+        inner join students on students.id = people.id
+        where people.id = '{zid}'
+        """
+    )
+    info = curs.fetchone()
+    curs.close()
+    return info or None
 
--- q3.py
+++ q3.py
@@ -4,57 +4,109 @@
 import sys
 import psycopg2
 import re
-from helpers import getProgram, getStream
+from helpers import get_program_info, get_stream_info
 
-# define any local helper functions here
-# ...
+def min_max_to_str(min_req, max_req):
+    if min_req and not max_req:
+        return f"at least {min_req}"
+    if not min_req and max_req:
+        return f"up to {max_req}"
+    if min_req and max_req and min_req < max_req:
+        return f"between {min_req} and {max_req}"
+    if min_req and max_req and min_req == max_req:
+        return f"{min_req}"
+    return ""  # min_req and max_req are null
 
-### set up some globals
-
-usage = f"Usage: {sys.argv[0]} (ProgramCode|StreamCode)"
-db = None
-
-### process command-line args
+def string_to_list(conn, string, query):
+    curs = conn.cursor()
+    array = string.split(",")
+    for item in array:
+        subitems = item.replace("{", "").replace("}", "").split(";")
+        for i, subitem in enumerate(subitems):
+            curs.execute(query + f"'{subitem}'")
+            name = curs.fetchone()[0]
+            if i > 0:
+                print(f"  or {subitem} {name}")
+            else:
+                print(f"- {subitem} {name}")
+    curs.close()
 
 argc = len(sys.argv)
 if argc < 2:
-  print(usage)
-  exit(1)
+    print(f"Usage: {sys.argv[0]} (ProgramCode|StreamCode)")
+    exit(1)
+
 code = sys.argv[1]
 if len(code) == 4:
-  codeOf = "program"
+    codeOf = "program"
 elif len(code) == 6:
-  codeOf = "stream"
+    codeOf = "stream"
 else:
-  print("Invalid code")
-  exit(1)
+    print("Invalid code")
+    exit(1)
+
+conn = psycopg2.connect("dbname=ass2")
+curs = conn.cursor()
 
 try:
-  db = psycopg2.connect("dbname=ass2")
-  if codeOf == "program":
-    progInfo = getProgram(db,code)
-    if not progInfo:
-      print(f"Invalid program code {code}")
-      exit(1)
-    #print(progInfo)  #debug
+    if codeOf == "program":
+        program_info = get_program_info(conn, code)
+        if not program_info:
+            print("Invalid program code", code)
+            exit()
 
-    # List the rules for Program
+        print(program_info[1], program_info[2])
+        print("Academic Requirements:")
 
-    # ... add your code here ...
+        curs.execute(
+            f"""
+            select * from programs
+            inner join requirements on requirements.for_program = programs.id
+            where programs.code = '{code}'
+            """
+        )
 
-  elif codeOf == "stream":
-    strmInfo = getStream(db,code)
-    if not strmInfo:
-      print(f"Invalid stream code {code}")
-      exit(1)
-    #print(strmInfo)  #debug
+    elif codeOf == "stream":
+        stream_info = get_stream_info(conn, code)
+        if not stream_info:
+            print("Invalid stream code", code)
+            exit()
 
-    # List the rules for Stream
+        print(stream_info[1], stream_info[2])
+        print("Academic Requirements:")
 
-    # ... add your code here ...
+        curs.execute(
+            f"""
+            select * from streams
+            inner join requirements on requirements.for_stream = streams.id
+            where streams.code = '{code}'
+            """
+        )
+
+    # Iterates through each requirement type.
+    for _, code, pname, _, rname, rtype, min_req, max_req, acadobjs, stream, program in curs.fetchall():
+        if rtype == 'core':
+            print(f"all courses from {rname}")
+            string_to_list(conn, acadobjs, "select title from subjects where code = ")
+        elif rtype == 'elective':
+            print(f"{min_max_to_str(min_req, max_req)} UOC courses from {rname}")
+            print("- " + acadobjs)
+        elif rtype == 'free':
+            print(f"{min_max_to_str(min_req, max_req)} UOC of {rname}")
+        elif rtype == 'gened':
+            print(f"{min_max_to_str(min_req, max_req)} UOC of {rname}")
+        elif rtype =='stream':
+            print(f"{min_max_to_str(min_req, max_req)} stream from {rname}")
+            string_to_list(conn, acadobjs, "select name from streams where code = ")
+        elif rtype == 'uoc':
+            print(f"Total UOC {min_max_to_str(min_req, max_req)} UOC")
+        else:
+            raise ValueError("Invalid requirement type")
 
 except Exception as err:
   print(err)
 finally:
-  if db:
-    db.close()
+    if curs:
+        curs.close()
+    if conn:
+        conn.close()
