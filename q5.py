678c99cacaa7e094260c785f6c748b9ce5eec2d2
dennuguyen <dennuguyen@gmail.com>
2023-11-16 05:16:03 +1100

    New approach.

--- q5.py
+++ q5.py
@@ -7,14 +7,64 @@ import psycopg2
 import re
 from helpers import get_latest_student, get_program, get_stream, get_program_requirements, get_academic_objects, get_full_transcript, get_stream_requirements
 
-def invert_dict_recursive(input_dict):
-    inverted_dict = {}
-    for rname, courses_list in input_dict.items():
-        for outer in courses_list:
-            for inner in outer:
-                course_code, course_name = inner
-                inverted_dict[course_code] = rname
-    return inverted_dict
+def process_requirements(requirements):
+    """
+    Convert requirements into dicts and acadobjs from string to lists so that
+    requirements are easier to use.
+    """
+
+    # Convert requirements from [namedtuple] into dict where rname is key.
+    requirements = { req.rname: req._asdict() for req in requirements }
+
+    # Convert acadobjs from strings to lists.
+    for rname, req in requirements.items():
+        if req["rtype"] in ["core", "elective"]:
+            req["acadobjs"] = get_academic_objects(conn, req["rtype"], req["acadobjs"])
+        if req["rtype"] in ["gened", "free"]:
+            req["acadobjs"] = [[(req["acadobjs"], rname)]]
+
+    return requirements
+
+def course_code_matcher(test, code):
+    return bool(re.match(test.replace("#", "."), code))
+
+def tick_off(requirements, course, rtype):
+    for rname, courses_list in requirements.items():
+        if courses_list["rtype"] == rtype:
+            acadobjs = courses_list["acadobjs"]
+            for outer in acadobjs:
+                for inner in outer:
+                    course_code, course_name = inner
+                    if rtype == "core":
+                        if course_code == course["code"]:
+                            acadobjs.remove(outer)
+                            course["rname"] = rname
+                            return True
+                    elif courses_list["min_req"] > 0:
+                        if course_code == course["code"]:
+                            acadobjs.remove(outer)
+                            courses_list["min_req"] -= course["uoc"]
+                            # courses_list["max_req"] -= course["uoc"]
+                            course["rname"] = rname
+                            return True
+                        if rtype in ["gened", "free"] or ("#" in course_code and course_code_matcher(course_code, course["code"])):
+                            courses_list["min_req"] -= course["uoc"]
+                            # courses_list["max_req"] -= course["uoc"]
+                            course["rname"] = rname
+                            return True
+    return False
+
+def tick_off_core(requirements, course):
+    return tick_off(requirements, course, "core")
+
+def tick_off_elective(requirements, course):
+    return tick_off(requirements, course, "elective")
+
+def tick_off_gened(requirements, course):
+    return tick_off(requirements, course, "gened")
+
+def tick_off_free(requirements, course):
+    return tick_off(requirements, course, "free")
 
 argc = len(sys.argv)
 if argc < 2:
@@ -38,12 +88,9 @@ if argc == 4:
     stream_code = sys.argv[3]
 
 conn = psycopg2.connect("dbname=ass2")
-req_buckets = {}
-req_quantities = {}
 
 try:
     stu_info = get_latest_student(conn, zid)
-    print(stu_info)
     if not stu_info:
         print(f"Invalid student id {zid}")
         exit(1)
@@ -58,81 +105,62 @@ try:
         print(f"Invalid program code {stream_code}")
         exit(1)
 
-    print(program_info)
+    # Print header.
+    print(f"{stu_info.zid} {stu_info.last_name}, {stu_info.first_name}")
+    print(f"{stu_info.program_code} {stu_info.stream_code} {stu_info.program_name}")
 
-    # Build requirement buckets from programs and stream.
+    # Get program requirements.
     program_requirements = get_program_requirements(conn, program_info.code)
+    program_requirements = process_requirements(program_requirements)
+
+    # Get stream requirements.
     stream_requirements = get_stream_requirements(conn, stream_info.code)
-    
-    print("\n\n\n")
-    for t in program_requirements:
-        print(t)
-    print("\n\n\n")
-
-    for t in stream_requirements:
-        print(t)
-    print("\n\n\n")
-
-    # Create requirement buckets.
-    for requirement in program_requirements + stream_requirements:
-        if requirement.rname not in req_buckets:
-            if requirement.rtype in ["core", "elective"]:
-                acadobjs = get_academic_objects(conn, requirement.rtype, requirement.acadobjs)
-                req_buckets[requirement.rname] = acadobjs
-                req_quantities[requirement.rname] = (requirement.min_req, requirement.max_req)
-            if requirement.rtype in ["gened", "free"]:
-                req_buckets[requirement.rname] = [[(requirement.acadobjs, requirement.rname)]]
-                req_quantities[requirement.rname] = (requirement.min_req, requirement.max_req)
+    stream_requirements = process_requirements(stream_requirements)
 
     transcript, achieved_uoc, wam = get_full_transcript(conn, stu_info.zid)
 
-    for k, v in req_buckets.items():
-        print(k, v)
-    print("\n\n\n")
-
-    # Invert the requirement buckets to allow look-up by subjects.
-    inverted_req_bucket = invert_dict_recursive(req_buckets)
-
     # Tick off requirements and attach requirements to each course on transcript.
+    # Brute force it.
     for course in transcript:
-        if "uoc" in course["course_uoc"]:
-            course["rname"] = inverted_req_bucket.get(course["code"], None)
-            if course["rname"]:
-                # Pop course from requirements bucket because done.
-                inverted_req_bucket.pop(course["code"])
-            else:
-                # Handle #### cases.
-                for code, rname in inverted_req_bucket.items():
-                    min_req, max_req = req_quantities[rname]
+        if "uoc" not in course["course_uoc"]:
+            continue
 
-                    # if is stream core
+        # Tick off stream then program cores.
+        if tick_off_core(stream_requirements, course):
+            continue
+        if tick_off_core(program_requirements, course):
+            continue
 
-                    # if is program core
+        # Tick off stream then program electives.
+        if tick_off_elective(stream_requirements, course):
+            continue
+        if tick_off_elective(program_requirements, course):
+            continue
 
-                    # if is stream elective
+        # Tick off general education.
+        if tick_off_gened(stream_requirements, course):
+            continue
+        if tick_off_gened(program_requirements, course):
+            continue
 
-                    # if is program elective
+        # Tick off free electives.
+        if tick_off_free(stream_requirements, course):
+            continue
+        if tick_off_free(program_requirements, course):
+            continue
 
-                    # if is general education
-
-                    # if is free elective
-
-                    # else:
-                        # "Could not be allocated"
-                        # course["uoc"] = "0uoc"
-                qty = req_quantities.get(course["rname"], 0)
-                print(course, qty)
+        course["rname"] = "Could not be allocated"
+        course["course_uoc"] = " 0uoc"
+        achieved_uoc -= course["uoc"]
 
     # Print transcript.
     for course in transcript:
-        print(f"{course.get('code')} {course.get('term')} {course.get('title'):<32.31s} {course.get('mark') or '-':>3} "
-                f"{course.get('grade') or '-':>2s}  {course.get('course_uoc') or ''}  {course.get('rname') or ''}")
+        print(f"{course['code']} {course['term']} {course['title']:<32.31s}"
+              f"{course['mark'] or '-':>3} {course['grade'] or '-':>2s}  {course.get('course_uoc', '')}  {course.get('rname', '')}")
+    print(f"UOC = {achieved_uoc}, WAM = {wam:2.1f}")
 
     # Print remaining progression.
-    if not inverted_req_bucket:
-        print("Eligible to graduate")
-    else:
-        pass
+    print("Eligible to graduate")
 
 except Exception:
     print(traceback.format_exc())
